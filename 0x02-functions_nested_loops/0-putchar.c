987a05bcfd222ef86f2914ffd5056b39d99b036d
lipnganga <philipmurage98@gmail.com>
2022-07-05 12:53:05 +0300

    putchar printed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/** main - prints putchar
-*Return: always 0
+/** 
+* main - prints putchar
+* Return: always 0
 */
 int main(void)
 {
@@ -12,7 +13,7 @@ int main(void)
 	_putchar('c');
 	_putchar('h');
 	_putchar('a');
-	_putchar('r')
+	_putchar('r');
 	_putchar('\n');
 	
 	return (0);
