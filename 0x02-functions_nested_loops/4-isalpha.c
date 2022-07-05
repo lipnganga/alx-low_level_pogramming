373e1e4aa6795a00c5c280a8cc69b52514793a3b
lipnganga <philipmurage98@gmail.com>
2022-07-05 14:45:24 +0300

    cheks for alphabetical letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,23 @@
 /**
 *test_isalpha -> Test the _isalpha function
 *
-*@n: Number to pass to _isalpha function
+*@c: Number to pass to _isalpha function
 */
-void test_isalpha(int n)
+void test_isalpha(int c)
 {
 	int r;
 
-	r = _isalpha(n);
+	r = _isalpha(c);
 	_putchar(r + '0');
 	_putchar('\n');
+}
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	test_isalpha('H');
+	return (0);
 }
