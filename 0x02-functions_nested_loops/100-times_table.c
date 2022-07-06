658d86e4d159c0dfc7d0fb994fc550e3b6b34450
lipnganga <philipmurage98@gmail.com>
2022-07-06 17:10:27 +0300

    print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,14 +5,7 @@
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
-
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+
 	return (0);
 }
