3d18bbe6d3f47ca7083c951da0e602bf15769cb0
lipnganga <philipmurage98@gmail.com>
2022-07-07 13:22:41 +0300

    print except 2 and 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9 and end with new line.
- *
- * Return: 0-9and new line after
- */
+* print_numbers -> print numbers
+*/
+
 void print_numbers(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-_putchar(i + '0');
-}
-_putchar('\n');
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_putchar(x + '0');
+	_putchar('\n');
 }
