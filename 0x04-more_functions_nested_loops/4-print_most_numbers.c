fec7ee3f42aca6e77213241c21975409a45f3ce8
lipnganga <philipmurage98@gmail.com>
2022-07-07 13:29:05 +0300

    fix it

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
-* print_most_numbers -> this prints most numbers
-*/
+ * print_most_numbers - print 0-9 and end with new line.
+ *
+ * Return: 0-9and new line after
+ */
 void print_most_numbers(void)
 {
-	int x;
-
-	for (x = 0; x < 10; x++)
-	{
-		if (x != 2 && x != 4)
-			_putchar(x + '0');
-	}
-	_putchar('\n');
+int i;
+for (i = 0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
 }
