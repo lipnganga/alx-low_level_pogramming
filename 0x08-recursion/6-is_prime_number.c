171ed83ddec0d55ebb397dfc30543c1ee775a141
lipnganga <philipmurage98@gmail.com>
2022-07-20 16:27:14 +0300

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,33 @@
 #include "main.h"
 
 /**
+ * primeFinder - searches for if a number is prime
+ *
+ * @a: the number to check
+ * @b: the numbers we'll go through
+ *
+ * Return: Whether or not the number is a prime number
+ */
+
+int primeFinder(int a, int b)
+{
+	if (a <= 1 || a % b == 0)
+		return (0);
+	else if (a == b)
+		return (1);
+	else if (a > b)
+		primeFinder(a, b + 1);
+
+	return (1);
+}
+
+/**
  * is_prime_number - prime identifier
  *@n: integer number
  *
  *Return: prime number result`
  */
 int is_prime_number(int n)
+{
+	return (primeFinder(n, 2));
+}
