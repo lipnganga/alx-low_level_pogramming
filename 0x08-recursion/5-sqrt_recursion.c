d9ab41af0d4d2258128f0f0cab557b6635f29e10
lipnganga <philipmurage98@gmail.com>
2022-07-20 16:12:25 +0300

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function to square a string  in recursion
+ *
+ *@n: - integer to be squared
+ *Return: Square result
+ */
+
+int _sqrt_recursion(int n)
+{
+		if (n == '\0')
+			return (0);
+		return (sqrt(n, 1));
+}
+
+int sqrt(int a, int b);
+/**
+ *sqrt - finds square root of two numbers
+ *
+ *@a: the number
+ *@b: number to tesst square root
+ *
+ * Return: square root
+ */
+
+int sqrt(int a, int b)
+{
+	if (b * b > a)
+		return (-1);
+	else if (b * b == a)
+		return (b);
+	else
+		return (sqrt(a, b + 1));
+	return (1);
+}
