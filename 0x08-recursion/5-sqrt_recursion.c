d1cb04018c6a6652cf73f43a693b3e4a54df555b
lipnganga <philipmurage98@gmail.com>
2022-07-20 16:22:38 +0300

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,36 +1,39 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function to square a string  in recursion
+ * helper - Finds the square root of two numbers
  *
- *@n: - integer to be squared
- *Return: Square result
- */
-
-int _sqrt_recursion(int n)
-{
-		if (n == '\0')
-			return (0);
-		return (sqrt(n, 1));
-}
-
-int _sqrt(int, int);
-/**
- *sqrt - finds square root of two numbers
- *
- *@a: the number
- *@b: number to tesst square root
+ * @a: The number
+ * @b: The number to test for the square root of @a
  *
  * Return: square root
  */
 
-int _sqrt(int a, int b)
+int helper(int a, int b)
 {
 	if (b * b > a)
 		return (-1);
+
 	else if (b * b == a)
 		return (b);
+
 	else
-		return (_sqrt(a, b + 1));
+		return (helper(a, b + 1));
+
 	return (1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: the number to return the square root of
+ *
+ * Return: the square root of @n
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,5 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
-
+int helper(int a, int b);
 #endif
