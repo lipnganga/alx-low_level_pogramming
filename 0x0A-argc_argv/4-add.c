4f80f2526120feba01383f9289380755739c86e2
lipnganga <philipmurage98@gmail.com>
2022-07-22 14:37:06 +0300

    add two positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Function to add two positive numbers
+ * @argc: argc first param
+ * @argv: an array of a command
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = 0, i, j;
+	
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+			printf("Error\n");
+				return (1);
+			}
+		}
+		num1 += atoi(argv[i]);
+	}
+	printf("%d\n", num1);
+	return (0);
+}
