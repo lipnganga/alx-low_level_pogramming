c1a4cac0c73d4a6f81ba7ea4764757544095893d
lipnganga <philipmurage98@gmail.com>
2022-07-28 13:11:15 +0300

    concatention of two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ *string_nconcat - two  string concatenation
+ *
+ *@s1: first string
+ *@s2: second string
+ *@n: number of bytes to include of @s2
+ *Return: new allocated space in memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int m, p, len;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+	if (j > n)
+		j = n;
+	len = i + j;
+
+	concat = malloc(sizeof(char) * (len + 1));
+	if (concat == NULL)
+		return (NULL);
+
+	for (p = 0; p < i; p++)
+		concat[k++] = s1[p];
+	for (m = 0; m < j; m++)
+		concat[k++] = s2[m];
+
+	concat[k] = '\0';
+	return (concat);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,5 +3,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+
+
 
 #endif
+
+
