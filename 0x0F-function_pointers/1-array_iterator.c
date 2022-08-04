d85e4f6ffc624298bf9e8436fdb2c7dc048723a1
lipnganga <philipmurage98@gmail.com>
2022-08-05 00:21:55 +0300

    execute function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,14 +11,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-		size_t i = 0;
-		void (*p)(int);
+	size_t i;
 
-		p = action;
-
-		if (!array || !action)
-			return;
-
+	if (array && action)
+	{
 		for (i = 0; i < size; i++)
-			p(array[i]);
+		{
+			action(array[i]);
+		}
+	}	
 }
