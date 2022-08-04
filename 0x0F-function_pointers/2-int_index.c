6d8b984667f62e2b713bff8a31d49dbb96bbb17d
lipnganga <philipmurage98@gmail.com>
2022-08-05 00:29:00 +0300

    execute function

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,22 +12,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 
-	int i;
-	int (*p)(int);
+	int i, r;
 
-	p = cmp;
-
-	if (size <= 0)
-		return (-1);
-	if (!array || !cmp)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (size > 0 && array && cmp)
 	{
-		if (p(array[i]) && array[i] != 0)
-			break;
-		if (i == size - 1)
-			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			r = cmp(array[i]);
+			if (r)
+				break;
+		}
+		if (i < size)
+			return (i);
 	}
-	return (i);
+	return (-1);
 }
