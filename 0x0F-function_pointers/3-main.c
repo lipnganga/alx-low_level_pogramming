c496c8084a96b5bc9ff2b0cc2b47f46892be4c0d
lipnganga <philipmurage98@gmail.com>
2022-08-05 00:49:18 +0300

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,64 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-/**
- * op_add - adds the ints
- * @a: first int
- * @b: second int
- * Return: sum
- */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-/**
- * op_sub - subtracts the ints
- * @a: first int
- * @b: second int
- * Return: difference
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
- * op_mul - multiplies the ints
- * @a: first int
- * @b: second int
- * Return: product
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
- * op_div - divides the ints
- * @a: first int
- * @b: second int
- * Return: result
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a / b);
-}
-/**
- * op_mod - remainder of division of ints
- * @a: first int
- * @b: second int
- * Return: remainder
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
-}
 
+/**
+ * main - performs simple operations
+ * @argc: number of arguments passed
+ * @argv: array of pointers to arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
+}
