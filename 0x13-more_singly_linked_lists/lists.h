bf28b0428b18ea8841417d3374b295d69bc33433
lipnganga <philipmurage98@gmail.com>
2022-08-16 20:50:45 +0300

    print all elements of a list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+#include <stdlib.h>
 
 /**
  *struct listint_s - singly linked list
@@ -15,6 +16,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
